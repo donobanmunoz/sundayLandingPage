e9114a3ad3f6798b8d676fca99f7a33598ecd119
donobanmunoz <dmunoztejeda@gmail.com>
2019-03-24 16:10:48 -0600

    partialWork

--- landingpage/src/App.js
+++ landingpage/src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-
+import './resource/styles.css';
+import Header from './components/header_footer/header.js'
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-    
+      <Header/>
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hola Zulma!!!!
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
